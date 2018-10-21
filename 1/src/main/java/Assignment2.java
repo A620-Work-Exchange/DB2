06fd113639e8870d440380aec512da4734153bad
2529716798 <2529716798@qq.com>
2018-10-21 15:51:51 +0800

    Update 1/src/main/java/Assignment2.java

--- 1/src/main/java/Assignment2.java
+++ 1/src/main/java/Assignment2.java
@@ -10,4 +10,57 @@ public class Assignment2 {
             "true&characterEncoding=utf-8&useSSL=false";
     private final static String USER = "root";
     private final static String PW = "1212hjkl";
+
+    long start = 0, end = 0;
+
+    private void init() {
+        try{
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USER, PW);
+            if(!connection.isClosed()) {
+                System.out.println("Succeed to connect...");
+
+
+            }else{
+                System.out.println("Fail to connect...");
+            }
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private void solution1() {
+        try {
+            Statement statement = connection.createStatement();
+            start = System.currentTimeMillis();
+            ResultSet resultSet = statement.executeQuery("select * from orders o where o.age < 20");
+            end = System.currentTimeMillis();
+            System.out.println("Solution 1 costs " + (end - start) + " milliseconds");
+            System.out.println("Solution 1 find " + getResNumber(resultSet) + " results...");
+         //   printOneColumn(resultSet, "S1");
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Assignment2 assignment2 = new Assignment2();
+        assignment2.init();
+        assignment2.solution1();
+    }
+
+    private long getResNumber(ResultSet resultSet) {
+        try {
+            long count = 0;
+            while (resultSet.next()) {
+                count += 1;
+            }
+            return count;
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return 0;
+    }
+
 }
