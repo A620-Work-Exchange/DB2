0ca7503671c8ca5822bfa22b3705b4a2478b19e2
2529716798 <2529716798@qq.com>
2018-10-22 20:56:41 +0800

    remove unnecessary depedency

--- 2/src/main/java/Main.java
+++ 2/src/main/java/Main.java
@@ -1,3 +1,10 @@
+import dao.BillDAO;
+import dao.UserDAO;
+
 public class Main {
+    private static UserDAO userDAO = new UserDAO();
+    public static void main(String[] args) {
+        userDAO.addUser("py", "123456");
+    }
 
 }
--- 2/src/main/java/dao/UserDAO.java
+++ 2/src/main/java/dao/UserDAO.java
@@ -1,4 +1,17 @@
 package dao;
 
+import domain.User;
+import util.HQLUtil;
+
 public class UserDAO {
+    public boolean addUser(String name, String password) {
+        User user = new User(name, password);
+        try {
+            HQLUtil.add(user);
+            return true;
+        }catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
 }
